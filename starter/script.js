c93bfbb8db7463c34e544562ccb9f2aa686ede71
Ehicodes <osaigbovoehinomen@gmail.com>
2025-04-24 15:27:09 +0100

    scoping in practice 1

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function calcAge(birthyear) {
+  const age = 2037 - birthyear;
+  console.log(firstName); //this works because the variableis a global variable an it works even if the variable was defined after the function, this is because the code in the function is only executed when it is actually called and the calling happens after the declarationof the firstName variable. So at this point  in the code the firstName variable is ready in the global execution environment in the global scope, ready to be used
+  return age;
+}
+
+const firstName = 'Ehi';
+calcAge(2006);
