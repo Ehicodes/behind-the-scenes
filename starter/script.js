59b235a873d401f7a8b68bc6dbc282a5c8d1084f
Ehicodes <osaigbovoehinomen@gmail.com>
2025-04-24 16:00:25 +0100

    scoping in practice 3

--- starter/script.js
+++ starter/script.js
@@ -6,12 +6,21 @@ function calcAge(birthyear) {
   function printAge() {
     const output = `${firstName} You are ${age}, born in ${birthyear}`;
     console.log(output);
+
+    //lets create a block(which generates a block scope)
+    if (birthyear >= 1981 && birthyear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    }
+    // console.log(str) we get a reference error because const and let are block-scoped so they  accesible or available only inside the block in which they were created
+    console.log(millenial); //it works because var is function-scoped   . we are still in the same function, so the scope of the millenial variable is the entire function no matter if it was declared inside of a block or not
   }
   printAge();
   return age;
 }
 
 const firstName = 'Ehi';
-calcAge(2006);
+calcAge(1991);
 // console.log(age);
 // printAge()  in the global scope, we do not have access to variables defined in any other scope
