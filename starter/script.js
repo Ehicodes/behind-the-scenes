9bec87cc330e3cc8980acb1bdf23a5926282b463
Ehicodes <osaigbovoehinomen@gmail.com>
2025-04-24 15:41:34 +0100

    scoping in practice 2

--- starter/script.js
+++ starter/script.js
@@ -2,9 +2,16 @@
 
 function calcAge(birthyear) {
   const age = 2037 - birthyear;
-  console.log(firstName); //this works because the variableis a global variable an it works even if the variable was defined after the function, this is because the code in the function is only executed when it is actually called and the calling happens after the declarationof the firstName variable. So at this point  in the code the firstName variable is ready in the global execution environment in the global scope, ready to be used
+
+  function printAge() {
+    const output = `${firstName} You are ${age}, born in ${birthyear}`;
+    console.log(output);
+  }
+  printAge();
   return age;
 }
 
 const firstName = 'Ehi';
 calcAge(2006);
+// console.log(age);
+// printAge()  in the global scope, we do not have access to variables defined in any other scope
